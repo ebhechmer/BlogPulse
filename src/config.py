7762a7ffc92be293d64998439a2f9ed9e2391ec7
Braden Hechmer <ehechmer@vols.utk.edu>
2024-08-28 22:24:17 -0400

    initial commit

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# twitter api config
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+
+# openai api key
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
